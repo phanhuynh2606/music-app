d9aeb4b816b83c9caebd19d5216a327f5c1a2e43
phanhuynh2606 <huynhpvhe170150@fpt.edu.vn>
2023-11-29 22:56:53 +0700

    Tạo route cơ bản |  Cài đặt PUG | Kết nối database

--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-console.log("Ok");
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import * as database from "./config/database";
+
+dotenv.config();
+
+database.connect();
+
+const app: Express = express();
+const port: string | number = process.env.PORT || 3000;
+
+// Setting PUG
+app.set("views","./views");
+app.set("view engine","pug");
+
+
+app.get("/topics", (req: Request, res: Response) => {
+  res.render("client/pages/topics/index");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
