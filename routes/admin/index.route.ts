84061a99b723a85df42f4dabe93e39f2b24b889e
Huynh Phan <huynhpvhe170150@fpt.edu.vn>
2023-12-16 05:26:20 +0700

    Tạo trang Quản lý bài hát

--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -3,10 +3,12 @@ import { Express, Request, Response } from 'express';
 import { dashboardRoute } from './dashboard.route';
 import { systemConfig } from '../../config/config';
 import { topicRoutes } from './topic.route';
+import { songRoutes } from './song.route';
 const PATH_ADMIN = `/${systemConfig.prefixAmin}`;
 const adminRoute = (app:Express) : void =>{
     app.use(`${PATH_ADMIN}/dashboard`, dashboardRoute);
     app.use(`${PATH_ADMIN}/topics`, topicRoutes);
+    app.use(`${PATH_ADMIN}/songs`, songRoutes);
     
 }
 
